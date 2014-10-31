8dd87102c3255f38b09ed62d019e287d1eb36cf5
LevanceWam <lkwamley@fullsail.edu>
2014-10-31 19:55:12 -0400

    Day 3 problems commit 18

--- Day3LabExpressions/js/script.js
+++ Day3LabExpressions/js/script.js
@@ -19,21 +19,21 @@ alert("Welcome to Day 3 problems page!");
 //Calculate the amount of pizza slices eaten by each person.
 var numberOfSlices = 8;
 var numberOfPeople = 20;
-var amountOrdered = 30
+var amountOrdered = 30;
 
 //Find the total amount of pizza slices.
 var slicesInAll = numberOfSlices * amountOrdered;
 //multiply numberofslices*amountOrdered
 var pizzaPerPerson = numberOfSlices * amountOrdered / numberOfPeople;
 
-console.log("There were " +slicesInAll+" "+"slices of pizza in total.")
+console.log("There were " +slicesInAll+" "+"slices of pizza in total.");
 
 //Now find the amount of pizza each person ate.
 
 //Divide totalPizzaSlices by numberOfPeople
 
 
-console.log("Each Person ate"+" "+pizzaPerPerson+" "+"slices of pizza.")
+console.log("Each Person ate"+" "+pizzaPerPerson+" "+"slices of pizza.");
 
 //Slice of pizza part 2
 
@@ -43,35 +43,38 @@ console.log("Each Person ate"+" "+pizzaPerPerson+" "+"slices of pizza.")
 
 var sparkySlices = numberOfSlices * amountOrdered % numberOfPeople;
 
-console.log("In total sparky had "+sparkySlices)
+console.log("In total sparky had "+sparkySlices);
 
 //Average Shopping Bill
-var week1 = 100
-var week2 = 50
-var week3 = 100
-var week4 = 50
-var week5 = 100
+var week1 = 100;
+var week2 = 50;
+var week3 = 100;
+var week4 = 50;
+var week5 = 100;
 
 //total amount spent = 5 week sum
 var totalAmountSpent = week1 + week2 + week3 + week4 + week5;
-console.log("Over 5 weeks I spent over $"+totalAmountSpent)
+console.log("Over 5 weeks I spent over $"+totalAmountSpent);
 
 //Find average for weekly spending
 var average = totalAmountSpent / 5; //5 is the number of weeks
-console.log("My 5 week average comes out to $"+average)
+console.log("My 5 week average comes out to $"+average);
 //Results format
 console.log("Over 5 weeks I spent $"+totalAmountSpent+" "+ "and my average is $"+average+" "+"per week.")
 
 //Discount
 var original = 20;
-console.log("The original price of the item is $"+original)
+console.log("The original price of the item is $"+original);
 var discount = 50;
-var discountperc = discount / 100
-console.log("The Discount is "+discount+"%")
-var description = "pack of gatorade";
-var salesTax = 4.5
+var discountperc = discount / 100;
+var description = "30 pack of gatorade";
+var salesTax = 50.5;
+var salesTaxPerc = salesTax / 100;
 
-var withtax
+
+var withoutTax = original * discountperc;
+var withTax = (withoutTax * salesTaxPerc) + withoutTax;
+console.log("After buying my"+" "+description+"which was originally"+" "+original+",but after a"+" "+discount+"%, "+"it is now $"+withoutTax+", without tax and $"+withTax+" "+"with tax")
 
 
 
