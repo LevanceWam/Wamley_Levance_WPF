2914a1b331643e096fe1d5ac2907158359c35330
LevanceWam <lkwamley@fullsail.edu>
2014-10-31 16:47:35 -0400

    day 3 problems commit1
    original is in web storm

---
+++ Day3LabExpressions/js/script.js
@@ -0,0 +1,19 @@
+/**
+ * Created by vancewamley on 10/31/14.
+ */
+
+//Levance Wamley
+
+//10/31/14
+
+//Web Programming Fundamentals
+
+//Section 1
+
+//Assignment: Instructional Expressions
+
+alert("Welcome to Day 3 problems page!");
+
+//Calculate the amount of pizza eaten at a party.
+var numberOfSlices = 8;
+var numberOfPeople = 20;
