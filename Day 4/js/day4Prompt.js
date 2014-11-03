381b388b57d82cc9efc60bf7a7fbc7dd629d4b74
LevanceWam <lkwamley@fullsail.edu>
2014-11-03 18:51:20 -0500

    Calculated age based on year
    learning about prompts

---
+++ Day 4/js/day4Prompt.js
@@ -0,0 +1,20 @@
+/*
+Levance Wamley
+Day 4 Prompts
+WPF 1411
+11-3-14
+ */
+//alert ("Testing 1,2,3")
+
+
+//ask user for output
+
+//prompt() askes the user to type something in
+var yearBorn = prompt("Enter your Year of birth here");
+console.log(yearBorn);
+
+//Figure out how old we are
+var age = 2014 - yearBorn;
+console.log("You are "+age+" years old.");
+
+
