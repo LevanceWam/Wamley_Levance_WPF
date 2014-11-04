6125d99735ae86746e329fc6e3b7de7ec4293e5a
LevanceWam <lkwamley@fullsail.edu>
2014-11-04 00:16:47 -0500

    Expression Assignment Commit 8

--- Expressions_Assignment/Expressions_Industry/expressionsIndustry.js
+++ Expressions_Assignment/Expressions_Industry/expressionsIndustry.js
@@ -5,4 +5,17 @@
  11-3-14
  */
 
-//alert("Testing");
+alert("Its the end of the month and you plan to make a list of the all the sites you plan to keep up?")
+//You go through your sites and find that you have 6 of them
+
+
+var numSites = 6;
+console.log("After going through your sites you have "+numSites)+" sites up and running.";
+//Now  list the sites
+var sites = ["Site 1", "Site 2", "Site 3", "Site 4", "Site 5", "Site 6"];
+console.log(sites);
+//You decide to check Site 5 because its a little buggy
+// access Site 5
+
+
+
