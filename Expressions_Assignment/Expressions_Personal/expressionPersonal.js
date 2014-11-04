0be0a4da54f49d3dd140257f14d7ebf684661621
LevanceWam <lkwamley@fullsail.edu>
2014-11-03 21:49:53 -0500

    Expression Assignment Commit 3

--- Expressions_Assignment/Expressions_Personal/expressionPersonal.js
+++ Expressions_Assignment/Expressions_Personal/expressionPersonal.js
@@ -4,14 +4,18 @@
  WPF 1411
  11-3-14
  */
-alert("Your local clothing store is having a sale.\n The sale goes as follows: for every individual article of clothing you buy you get 20 dollars off the item.");
+alert("Your local clothing store is having a sale.\n The sale goes as follows: for every individual article of clothing over $40.You get 20 dollars off the item.");
 
 
 //Your local clothing store is having a sale.
 //The sale goes as follows: for every individual article of clothing you buy you get 20 dollars off the item.
 var item1 = prompt("What is the price of item 1");
+console.log(item1);
 var item2 = prompt("What is the price of item 2");
+console.log(item2);
 var item3 = prompt("What is the price of item 3");
+console.log(item3);
 var item4 = prompt("What is the price of item 4");
+console.log(item4);
 
-var checkOut
+var checkOut = Number(item1) + Number(item2) + parseInt(item3) + parseInt(item4);
