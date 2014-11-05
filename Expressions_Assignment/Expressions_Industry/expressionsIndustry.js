98113f4ce5f4f7551ede6c97e2ea68682dbb0d2a
LevanceWam <lkwamley@fullsail.edu>
2014-11-05 15:05:05 -0500

    Expression Assignment Commit 21

--- Expressions_Assignment/Expressions_Industry/expressionsIndustry.js
+++ Expressions_Assignment/Expressions_Industry/expressionsIndustry.js
@@ -11,6 +11,7 @@ alert("Its the end of the month and you plan to make a list of the all the sites
 
 
 var numSites = 6;
+alert("After going through your sites you have "+numSites)+" sites up and running.";
 console.log("After going through your sites you have "+numSites)+" sites up and running.";
 //Now  list the sites
 var sites = ["Site 1", "Site 2", "Site 3", "Site 4", "Site 5", "Site 6"];
@@ -38,4 +39,11 @@ var mean = [site1, site2, site3, site4, site5 , site6];
 //now find out the mean total by adding them all together
 
 var meanTotal = Number(mean[0]) + Number(mean[1]) + Number(mean[2]) + Number(mean[3]) + Number(mean[4]) + Number(mean[5]);
+alert("After adding all of the sites together your added total is "+ meanTotal);
 console.log("After adding all of the sites together your added total is "+ meanTotal);
+
+// divide by total number of sites
+alert("Now we have to divide the total by the number of sites");
+var finalMean = meanTotal/mean.length;
+alert("After dividing your added total by 6 we now have the mean of all of your sites and that is  "+finalMean);
+console.log(finalMean)
