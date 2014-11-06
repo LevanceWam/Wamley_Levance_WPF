5b1b10fd3844540d6e80f6d48a1d0323729cd950
LevanceWam <lkwamley@fullsail.edu>
2014-11-05 23:34:18 -0500

    Conditionals Assignment commit 8

--- Conditionals_Assignment/Group 3/js/group3.js
+++ Conditionals_Assignment/Group 3/js/group3.js
@@ -6,3 +6,18 @@ WPF 1411
 11-5-2014
  */
 //alert(hello);
+
+ var age =prompt("How old are you");
+console.log(age);
+
+
+ if (age < 10){
+console.log(" The child's ticket is $7.00");
+
+ } else if (age >= 55){
+  console.log("The ticket for you is $7.00");
+
+ } else {
+  console.log("The ticket for you is $12.00")
+ }
+
