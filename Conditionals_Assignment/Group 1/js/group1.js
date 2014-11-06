68dd728050a6508e280cbef69f06dd4ff7810bc7
LevanceWam <lkwamley@fullsail.edu>
2014-11-05 22:55:09 -0500

    Conditionals Assignment commit 3
    working on group 2

--- Conditionals_Assignment/Group 1/js/group1.js
+++ Conditionals_Assignment/Group 1/js/group1.js
@@ -6,4 +6,26 @@ WPF 1411
  */
 //alert ("hello");
 
- //
+ /* Celsius to Fahrenheit converter
+ Celsius
+ The Formula to convert celsius to fahrenheit
+ is x * 1.8(this is 9/5 in decimal form) + 32
+ fahrenheit
+ the formula to do this conversion is
+ y * .5556(this is 5/9 in decimal form) - 32
+  */
+
+var x =prompt("What is the temperature outside");
+ console.log("It is currently "+x+" outside");
+
+ if(x = x+"F"){
+  console.log("The Temperature in Celsius is "+x)
+
+ } else if (x+"F"*.5556-32){
+console.log(x)
+
+ }
+
+
+
+
--- Conditionals_Assignment/Group 2/js/group 2.js
+++ Conditionals_Assignment/Group 2/js/group 2.js
@@ -6,3 +6,13 @@ WPF 1411
 11-5-2014
  */
 //alert("hello2");
+
+
+ var grade =prompt("what is your grade");
+
+ if (grade <= 69){
+  console.log("Sorry "+grade+" is a F")
+ }
+ else if (){
+  console.log
+ }
