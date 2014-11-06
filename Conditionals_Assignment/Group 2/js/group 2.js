98e51c719ecd7d83267bfed326ad915be8a4a938
LevanceWam <lkwamley@fullsail.edu>
2014-11-05 23:05:05 -0500

    Conditionals Assignment commit 5
    I have to fill in the other numbers for  group 2

--- Conditionals_Assignment/Group 2/js/group 2.js
+++ Conditionals_Assignment/Group 2/js/group 2.js
@@ -11,8 +11,32 @@ WPF 1411
  var grade =prompt("what is your grade");
 
  if (grade <= 69){
-  console.log("You have a "+grade+ " , which means you have earned a(n) F in the class ")
- }
- else if (grade <= 72){
-  console.log("You have a "+grade+ " , which means you have earned a(n) D in the class ")
+  console.log("You have a "+grade+ " , which means you have earned a(n) F in the class ");
+
+ } else if (grade <= 72){
+  console.log("You have a "+grade+ " , which means you have earned a(n) D in the class ");
+
+ } else if (grade <= 75) {
+  console.log("You have a " + grade + " , which means you have earned a(n) C in the class ");
+
+ } else if (grade <= ) {
+  console.log("You have a " + grade + " , which means you have earned a(n)  in the class ");
+
+ } else if (grade <= ) {
+  console.log("You have a " + grade + " , which means you have earned a(n)  in the class ");
+
+ } else if (grade <= ) {
+  console.log("You have a " + grade + " , which means you have earned a(n)  in the class ");
+
+ } else if (grade <= ) {
+  console.log("You have a " + grade + " , which means you have earned a(n)  in the class ");
+
+ } else if (grade <= ) {
+  console.log("You have a " + grade + " , which means you have earned a(n)  in the class ");
+
+ } else if (grade <= ) {
+  console.log("You have a " + grade + " , which means you have earned a(n)  in the class ");
+
+ } else {
+  console.log("You need to get to work if you're seeing this message.")
  }
