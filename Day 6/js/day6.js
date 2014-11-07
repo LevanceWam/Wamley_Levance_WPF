4e3e4cde54ad5df8583cebba5bf7bf6526dfa7dd
LevanceWam <lkwamley@fullsail.edu>
2014-11-07 18:22:41 -0500

    nesting if statements
    finished

--- Day 6/js/day6.js
+++ Day 6/js/day6.js
@@ -16,28 +16,64 @@ WPF 1411
 
 
  //Get temp
- var temp =69;
+ var temp =prompt("what is the temperature out side");
 
  //get water temp
 
  var waterTemp = 60;
 
+
+
+ //are you taking a kid
+ var child = true;
+
  //if else statement
 
  if(temp>70){
   console.log("lets go to the beach");
+//test water temp
+  waterTemp =prompt("what is the water temp");
 
+  if(waterTemp>75){
+   console.log("lets go swimming in the water");
+
+  } else {
+   console.log("lets lay out and get a tan");
+
+  }
  } else {
-  console.log("lets go to the movies.")
+  console.log("lets go to the movies.");
+
+
+  if(child){
+   var childAge = prompt("how old is the child");
+   if (childAge<=6){
+
+    console.log("watch the smurf movies");
+   } else{
+   console.log("go watch evil within");
+  }
+
 
  }
 
- //test water temp
 
- if(waterTemp>75){
-  console.log("lets go swimming in the water")
 
- } else {
-  console.log("lets lay out and get a tan")
+ // using && and ||
 
- }
+ if(temp>70 && waterTemp>75){
+  console.log("lets go to the beach\n lets go swimming");
+
+ } else if(temp>70 && waterTemp<=75){
+  console.log("lets go to the beach\n and lets get a tan");
+
+ } else if(temp<=75 && child<=6){
+  console.log("go to movie with kid");
+ } else if(temp<=70 && !child<=6){
+  console.log("bring the blood");
+
+ }}
+
+
+
+
