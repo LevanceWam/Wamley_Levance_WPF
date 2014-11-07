605f1fa10e6e0a7ef9a0b49758855b32bb8cdb2f
LevanceWam <lkwamley@fullsail.edu>
2014-11-07 17:36:38 -0500

    nesting if statements
    wrong way so far

---
+++ Day 6/js/day6.js
@@ -0,0 +1,43 @@
+
+ /*
+Levance Wamley
+Nesting If Statements
+WPF 1411
+11--2014
+ */
+
+ //alert("ello gov");
+
+
+ //Some decisions depend on other decisions
+
+ // if it is warm lets go to the beach if not lets go to the movies
+ // if the water is warm enough lets go swimming if not tan
+
+
+ //Get temp
+ var temp =69;
+
+ //get water temp
+
+ var waterTemp = 60;
+
+ //if else statement
+
+ if(temp>70){
+  console.log("lets go to the beach");
+
+ } else {
+  console.log("lets go to the movies.")
+
+ }
+
+ //test water temp
+
+ if(waterTemp>75){
+  console.log("lets go swimming in the water")
+
+ } else {
+  console.log("lets lay out and get a tan")
+
+ }
