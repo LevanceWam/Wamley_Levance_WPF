e7bda161d8f7e64eb6198a09a5712961306ad9cb
LevanceWam <lkwamley@fullsail.edu>
2014-11-07 23:21:26 -0500

    Conditionals Assignment Commit 4

--- Conditionals_Assignments/Conditionals_Industry/js/conditionalsIndustry.js
+++ Conditionals_Assignments/Conditionals_Industry/js/conditionalsIndustry.js
@@ -5,3 +5,17 @@ Title
 WPF 1411
 11--2014
  */
+ alert("You are at the local veteran and the doctor asks how many months has it been since your pet has had a check up.\n" +
+ "4 months means a quick check up and anything past 8 months is terrible");
+
+ var lastVisit =prompt("When was the last time you took your pet to the veteran.");
+ if (lastVisit<=4){
+  console.log("Please take your pet to the doctor for a quick follow up");
+
+ } else if(lastVisit>=8){
+  console.log("You need to take your pet to the vet more often");
+
+ } else{
+  console.log("Your pet is doing fine! Please continue to bring it to get it checked out");
+
+ }
--- Conditionals_Assignments/Conditionals_Personal/js/conditionalsPersonal.js
+++ Conditionals_Assignments/Conditionals_Personal/js/conditionalsPersonal.js
@@ -7,7 +7,14 @@ WPF 1411
  */
 
 
- //You're at a friend's house playing basketball the game is close and you have to make a split decision on draining the three pointer or 2 pointer
- // the score is 98 - 101
- // you have the ball
- //
+ //you have to take your pet to the vet
+ // you base your visits to the vet as shown below
+ // if you haven't gotten a check up past 8 months
+ // or if you recently went in the past 4 months you go for a quick follow up.
+
+ //set a var for the last and next visit
+
+
+ //ternary
+
+ (lastVisit<=4) ? console.log("Please take your pet to the doctor for a quick follow up") : (lastVisit>=8)? console.log("You need to take your pet to the vet more often") : console.log("Your pet is doing fine! Please continue to bring it to get it checked out");
