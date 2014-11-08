4a458f0cc1e014c91e7a1af8c7d29e365642d89c
LevanceWam <lkwamley@fullsail.edu>
2014-11-07 20:25:27 -0500

    finished while loop starting for loop

--- Day 7/js/day7WhileLoop.js
+++ Day 7/js/day7WhileLoop.js
@@ -5,3 +5,37 @@ Title
 WPF 1411
 11--2014
  */
+//alert (elle)
+
+ //basic while loop
+ // the while loop will loop through a block of code as long as the condition mains true
+
+  var counter = 0; // step one inititailize the counter variable
+
+ while (counter<20){//condition will run as long as this is true
+  console.log("somethings will never end ");
+
+  //update or change the counter variable
+  counter++;
+
+ }
+ console.log("end of loop - last counter variable "+counter);
+
+
+
+
+ //do while loop
+ //its sort of like the while loop
+ //difference is the code will at least run once
+
+
+ var i = 0;//counter variable
+
+ do{
+  console.log("the counting number is "+ i);
+  //update or change the counting varible
+i+=5;
+
+ }while(i<50);
+
+
