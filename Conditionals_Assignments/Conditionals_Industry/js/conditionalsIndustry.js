c50f25cc92760c6b2c086983fd03b30ce3044143
LevanceWam <lkwamley@fullsail.edu>
2014-11-10 11:56:37 -0500

    Conditionals Assignment Commit 11

--- Conditionals_Assignments/Conditionals_Industry/js/conditionalsIndustry.js
+++ Conditionals_Assignments/Conditionals_Industry/js/conditionalsIndustry.js
@@ -9,5 +9,7 @@ WPF 1411
  //You have a ton of websites you need to have done.
  //if you have a certain amount done you can have a couple of days off before the due date because your a couple days ahead of schedule.
  // you have 50 sites
- // you have  19 days to get the job done
- // if you have
+ // you have 12 days to get the job done
+ // if you have at least 4 or 6 sites done within 6 - 9 days you can have some days off
+
+ var days
