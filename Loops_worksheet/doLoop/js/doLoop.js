fde2584266cfbd2cde4bf544c219aadeeda8dc2f
LevanceWam <lkwamley@fullsail.edu>
2014-11-10 22:59:04 -0500

    Loop Worksheet commit 6

--- Loops_worksheet/doLoop/js/doLoop.js
+++ Loops_worksheet/doLoop/js/doLoop.js
@@ -8,7 +8,11 @@ WPF 1411
 
  //alert("ello");
 
-//You are trying to help your child learn their multiplication table.
- //the way this is suppose to work is when you type in a number it should output the mu
+ //You are trying to help your child learn their multiplication table.
+ //the way this is suppose to work is when you type in a number it should output the multiplication table for the number.
+ //this table goes up to the selected number version of 56 because we do not want to overload your child
+ //for example
+
+ var num=prompt("choose a number you want to see the multiplication table for.");
 
 
