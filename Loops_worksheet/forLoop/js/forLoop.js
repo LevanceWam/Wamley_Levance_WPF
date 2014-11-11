54e42b4353caa95c927ad2777de76ae0c728291f
LevanceWam <lkwamley@fullsail.edu>
2014-11-10 22:53:22 -0500

    Loop Worksheet commit 4

--- Loops_worksheet/forLoop/js/forLoop.js
+++ Loops_worksheet/forLoop/js/forLoop.js
@@ -5,3 +5,14 @@ Title
 WPF 1411
 11--2014
  */
+ //You are trying to help your child learn their multiplication table.
+ //the way this is suppose to work is when you type in a number it should output the multiplication table for the number.
+ //this table goes up to the selected number version of 56 because we do not want to overload your child
+ //for example
+
+ var num=prompt("choose a number you want to see the multiplication table for.");
+
+ for(var i=num ; i<=56; i+=num){
+console.log(i)
+
+ }
