27e4beb373599530c0643dfbe94d1f12db8010d8
LevanceWam <lkwamley@fullsail.edu>
2014-11-12 14:12:50 -0500

    Loop Worksheet commit 10

--- Loops_worksheet/whileLoop/js/whileLoop.js
+++ Loops_worksheet/whileLoop/js/whileLoop.js
@@ -1,7 +1,12 @@
 
  /*
 Levance Wamley
-Title
+While loops
 WPF 1411
-11--2014
+11-10-2014
  */
+
+alert("Hey sleepy head we know that you are tired so we decide to count sheep to help you sleep")
+ //we are going to count sheep until you fall asleep!
+  var counter = 0;
+ var sheep =prompt("How many sheep do you want to count to?");
