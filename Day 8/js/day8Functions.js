8e207a104a09b18a50c2cad889f44195cbc2a326
LevanceWam <lkwamley@fullsail.edu>
2014-11-12 18:40:35 -0500

    starting files scope
    finished functions

--- Day 8/js/day8Functions.js
+++ Day 8/js/day8Functions.js
@@ -1,7 +1,58 @@
 
  /*
 Levance Wamley
-Title
+Functions
 WPF 1411
-11--2014
+11-12-2014
  */
+
+ //alert
+ //function call
+ calcArea();
+
+ //create a function = console.log hello
+
+ //function functionName(){}
+
+ function printHello(){
+
+
+  //inside the function
+  console.log("hello")
+
+ }
+ //create a second function that prints more
+ function printMore(){
+
+
+  console.log("Print out more text");
+
+ }
+
+
+ //function call - "go"button of the factory
+ //functionName();
+
+ printHello();
+ printMore();
+ printHello();
+
+ printMore();
+
+
+
+
+ //create a function that will calculate the area of a rectangle
+
+ function calcArea(){
+
+
+  //create the variable
+
+  var width = 20;
+  var height = 30;
+  var area= width*height;
+
+  console.log("Your rectangle has an area of "+area)
+
+ }
