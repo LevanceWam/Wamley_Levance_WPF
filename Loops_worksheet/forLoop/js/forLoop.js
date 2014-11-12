68a77937966d4dec6b04da07b93337e3158f4097
LevanceWam <lkwamley@fullsail.edu>
2014-11-12 17:17:42 -0500

    Loop Worksheet commit 19
    \

--- Loops_worksheet/forLoop/js/forLoop.js
+++ Loops_worksheet/forLoop/js/forLoop.js
@@ -8,27 +8,16 @@ WPF 1411
 //You are playing a game on your phone for every time you log on for as long as  you get 4 coins
  //you start with 20
 
-var days = 60;
- var coins = 4;
+var days = prompt("How many days do you plan on playing this game in a row");
+ var coins = 20;
+ var nextLogin = 4;
 
- for(var i =1; i<=days;i++){
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ for(var i=1; i<=days;i++){
+var newCoins = (coins)+(nextLogin);
+ } if(days===0) {
+  console.log("stop playing the game then.")
+ }
+ else if(days==="" || isNaN(days)) {
+days=prompt("enter a value only")
+  }
  }
