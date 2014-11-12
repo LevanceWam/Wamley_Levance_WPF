068bc4c61737bd283805d594bd0b69b7b29d4b1d
LevanceWam <lkwamley@fullsail.edu>
2014-11-12 14:48:34 -0500

    Loop Worksheet commit 17

--- Loops_worksheet/whileLoop/js/whileLoop.js
+++ Loops_worksheet/whileLoop/js/whileLoop.js
@@ -12,14 +12,22 @@ alert("Hey sleepy head we know that you are tired so we decide to count sheep to
  var sheep =prompt("How many sheep do you want to count to?");
 
  while (counter<=sheep){
-console.log(counter+" sheeps");
 
-  if(counter===1){
+  while(sheep==="" || isNaN(sheep)){
+   sheep=prompt("Please try again and make sure that your number is greater than 0 and there is a value.");
+
+  }if(sheep===0){
+   sheep=prompt("Please try again and make sure that your number is greater than 0 and there is a value.");
+
+  }else if(counter===0) {
+   console.log(counter + " sheep");
+
+
+  }else if(counter===1){
    console.log(counter+" sheep")
+  } else{
+   console.log(counter+" sheeps");
 
-
-  } if(sheep===0 || sheep==="" || isNaN(sheep)){
-sheep=prompt("Please try again and make sure that your number is greater than 0 and there is a value.");
 
   }counter++;
- }
+  }
