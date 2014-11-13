a5c4f141f976c56e3aff2961632bca8d62c1936c
LevanceWam <lkwamley@fullsail.edu>
2014-11-12 21:44:16 -0500

    Functions worksheet Commit 1

---
+++ Functions_Worksheet/js/Circumference.js
@@ -0,0 +1,25 @@
+
+ /*
+Levance Wamley
+Circumference
+WPF 1411
+11-12-2014
+ */
+//find the circumference of a circle
+
+ function circumference (cir){
+var radius =prompt("")
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
