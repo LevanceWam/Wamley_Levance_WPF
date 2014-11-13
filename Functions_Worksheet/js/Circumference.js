0e7db35d6ecef2bea0bb927108155a7aa1b5eb9c
LevanceWam <lkwamley@fullsail.edu>
2014-11-12 22:37:02 -0500

    Loop Worksheet commit 6

--- Functions_Worksheet/js/Circumference.js
+++ Functions_Worksheet/js/Circumference.js
@@ -6,29 +6,20 @@ WPF 1411
 11-12-2014
  */
 //find the circumference of a circle
-
+ var radius =prompt("What is the Radius:");
  function circumference (radius){
-var radius =prompt("What is the Radius:");
+
 
 var cirCu = 2*Math.PI*radius;
 
-console.log("The circumference of the circle is "+cirCu);
-validateThis(radius)
+//console.log("The circumference of the circle is "+cirCu);
 
-
-
-
-
-
-
-
-
-
-
-
+  return cirCu;
  }
+ radius=validateThis(radius);
+var returnedCir =circumference(radius);
 
-
+console.log("The circumference of the circle is "+returnedCir);
 
  function validateThis(userPrompt){
   while(userPrompt==="" || isNaN(userPrompt)){
