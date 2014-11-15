7bace85bdbe5cc4995c643cdd16571db44a42565
LevanceWam <lkwamley@fullsail.edu>
2014-11-14 22:06:13 -0500

    Functions Assignments commit 3

--- Function_Assignments/Functions_Personal/js/functionsPersonal.js
+++ Function_Assignments/Functions_Personal/js/functionsPersonal.js
@@ -3,5 +3,25 @@
 Levance Wamley
 Title
 WPF 1411
-11--2014
+11-14-2014
  */
+
+ //Its party night. You and a couple of friends can't decide on whether to go to
+ // Longhorn, Red Lobster, or Buffalo Wild Wings
+ //So out of 10 people you guys take a vote.
+
+ var longhorn=prompt("How many people voted for Longhorn?");
+ var redLobster=prompt("How many people voted for Red Lobster?");
+ var buffaloWildWings=prompt("How many people voted for Buffalo Wild  Wings?");
+
+ var restuarant =function(lh,rl,bww){
+
+  if(lh>rl && lh>bww){
+   console.log("Let's go to Longhorn")
+
+
+  }
+
+
+ };
+
